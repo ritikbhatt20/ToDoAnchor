459715749b2d3ddfbe0f6497639d27937c04cf86
ritikbhatt20 <ritikbhatt020@gmail.com>
2024-04-30 18:50:49 +0530

    ⛏️Initialize user fn done

--- programs/to-do-contract/src/lib.rs
+++ programs/to-do-contract/src/lib.rs
@@ -1,15 +1,40 @@
 use anchor_lang::prelude::*;
 
 pub mod constant;
-pub mod error;
+// pub mod error;
 pub mod states;
-use crate::{constant::*, error::*, states::*};
+use crate::{constant::*, states::*};
 
-declare_id!("11111111111111111111111111111111");
+declare_id!("9GZrJjRysApW8xCfonMqywgJu6kGVvLF8LCiXwBKgsj5");
 
 #[program]
 pub mod todo {
     use super::*;
-    
+    pub fn initialize_user(ctx: Context<InitializeUser>) -> Result<()> {
+        let user_profile = &mut ctx.accounts.user_profile;
+        
+        user_profile.authority = ctx.accounts.authority.key();
+        user_profile.todo_count = 0;
+        user_profile.last_todo = 0;
+
+        Ok(())
+    }
 }
 
+#[derive(Accounts)]
+#[instruction()]
+pub struct InitializeUser<'info> {
+    #[account(mut)]
+    pub authority: Signer<'info>,
+
+    #[account(
+        init,
+        seeds = [USER_TAG, authority.key().as_ref()],
+        bump,
+        payer = authority,
+        space = 8 + std::mem::size_of::<UserProfile>(),
+    )]
+    pub user_profile: Box<Account<'info, UserProfile>>,
+
+    pub system_program: Program<'info, System>,
+}
