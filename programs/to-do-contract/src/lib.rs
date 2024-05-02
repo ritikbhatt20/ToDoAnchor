7e923d2740fbd9c05877a6303b1ef7d3156f5bc2
ritikbhatt20 <ritikbhatt020@gmail.com>
2024-05-02 16:41:26 +0530

    🎯Mark todo fn done

--- programs/to-do-contract/src/lib.rs
+++ programs/to-do-contract/src/lib.rs
@@ -1,9 +1,9 @@
 use anchor_lang::prelude::*;
 
 pub mod constant;
-// pub mod error;
+pub mod error;
 pub mod states;
-use crate::{constant::*, states::*};
+use crate::{constant::*, error::*, states::*};
 
 declare_id!("9GZrJjRysApW8xCfonMqywgJu6kGVvLF8LCiXwBKgsj5");
 
@@ -40,7 +40,17 @@ pub mod todo {
         Ok(())
     }
 
-    pub fn mark_todo(ctx: Context<AddTodo>, todo_idx: u8) -> Result<()> {
+    pub fn mark_todo(ctx: Context<MarkTodo>, todo_idx: u8) -> Result<()> {
+        let todo_account = &mut ctx.accounts.todo_account;
+
+        require!(!todo_account.marked, TodoError::AlreadyMarked);
+
+        todo_account.marked = true;
+
+        Ok(())
+    }
+
+    pub fn remove_todo(ctx: Context<RemoveTodo>, todo_idx: u8) -> Result<()> {
         
     }
 }
@@ -87,4 +97,42 @@ pub struct AddTodo<'info> {
     pub authority: Signer<'info>,
 
     pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(todo_idx: u8)]
+pub struct MarkTodo<'info> {
+    #[account(
+        mut,
+        seeds = [USER_TAG, authority.key().as_ref()],
+        bump,
+        has_one = authority,
+    )]
+    pub user_profile: Box<Account<'info, UserProfile>>,
+
+    #[account(
+        mut,
+        close = authority,
+        seeds = [TODO_TAG, authority.key().as_ref(), &[todo_idx].as_ref()],
+        bump,
+        has_one = authority,
+    )]
+    pub todo_account: Box<Account<'info, ToDoAccount>>,
+
+    #[account(mut)]
+    pub authority: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(todo_idx: u8)]
+pub struct RemoveTodo<'info> {
+    #[account(
+        mut,
+        seeds = [USER_TAG, authority.key().as_ref()],
+        bump,
+        has_one = authority,
+    )]
+    pub user_profile: Box<Account<'info, UserProfile>>,
 }
